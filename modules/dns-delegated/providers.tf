391dc0d7159db864f0d6013a11aaa9c39df71d04
Joe Niland <joe@originalmind.com.au>
2021-04-07 14:28:51 +1000

    dns-delegated: add module

---
+++ modules/dns-delegated/providers.tf
@@ -0,0 +1,19 @@
+# provider "aws" {
+#   # The AWS provider to use to make changes in the DNS primary account
+#   alias  = "primary"
+#   region = var.aws_region
+
+#   #   assume_role {
+#   #     role_arn = coalesce(var.import_role_arn, module.iam_roles.dns_terraform_role_arn)
+#   #   }
+# }
+
+provider "aws" {
+  # The AWS provider to use to make changes in the target (delegated) account
+  alias  = "delegated"
+  region = var.aws_region
+
+  assume_role {
+    role_arn = format("arn:aws:iam::${var.aws_account_id}:role/${var.delegated_role_name}") # coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
+  }
+}
---
+++ modules/dns-delegated/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "AWS Account ID of target (delegated) account"
+}
+
+variable "zone_config" {
+  description = "Zone config"
+  type = list(object({
+    subdomain = string
+    zone_name = string
+  }))
+}
+
+variable "delegated_role_name" {
+  type        = string
+  description = "Role name in target account to use for setting up delegated DNS zone"
+}
