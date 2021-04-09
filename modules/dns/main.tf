a4aac413c5290c0163d14800ee73d2af75012874
Joe Niland <joe@originalmind.com.au>
2021-04-09 14:02:44 +1000

    dns: handle existing zone

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,30 +1,30 @@
 locals {
-  zone_name = var.hosted_zone_name == "" ? var.cert_domain_name : var.hosted_zone_name
-  # zone_name_in_use = var.create_hosted_zone ? join("", aws_route53_zone.default.*.name) : join("", data.aws_route53_zone.existing.*.name)
+  zone_name        = var.hosted_zone_name == "" ? var.cert_domain_name : var.hosted_zone_name
+  zone_name_in_use = var.create_hosted_zone ? join("", aws_route53_zone.default.*.name) : join("", data.aws_route53_zone.existing.*.name)
 }
 
 resource "aws_route53_zone" "default" {
-  count   = module.this.enabled && var.create_hosted_zone ? 1 : 0
+  count   = (module.this.enabled && var.create_hosted_zone) ? 1 : 0
   name    = local.zone_name
   comment = format("DNS zone for %s", local.zone_name)
   tags    = module.this.tags
 }
 
-# data "aws_route53_zone" "existing" {
-#   count = var.create_hosted_zone ? 0 : 1
-#   name  = local.zone_name
-# }
+data "aws_route53_zone" "existing" {
+  count = ! var.create_hosted_zone ? 1 : 0
+  name  = local.zone_name
+}
 
 module "acm_request_certificate" {
-  source                            = "git::https://github.com/cloudposse/terraform-aws-acm-request-certificate.git?ref=tags/0.9.0"
+  source                            = "git::https://github.com/cloudposse/terraform-aws-acm-request-certificate.git?ref=tags/0.13.1"
   enabled                           = module.this.enabled
   domain_name                       = var.cert_domain_name
   process_domain_validation_options = var.auto_verify
   ttl                               = "300"
   subject_alternative_names         = compact(concat(formatlist("%s.%s", var.alternative_domain_prefixes, var.cert_domain_name), var.alternative_domains))
   wait_for_certificate_issued       = var.wait_for_certificate_issued
-  zone_name                         = join("", aws_route53_zone.default.*.name)
-  # local.zone_name_in_use # var.create_hosted_zone ? join("", aws_route53_zone.default.*.name) : join("", data.aws_route53_zone.existing.*.name)
+  # zone_name                         = join("", aws_route53_zone.default.*.name)
+  zone_name = local.zone_name_in_use # var.create_hosted_zone ? join("", aws_route53_zone.default.*.name) : join("", data.aws_route53_zone.existing.*.name)
 
   depends_on = [
     aws_route53_zone.default
