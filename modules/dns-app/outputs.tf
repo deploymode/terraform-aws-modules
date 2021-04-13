34bb54c3e768d56d733cdb7a242cf705af473d45
Joe Niland <joe@originalmind.com.au>
2021-04-13 11:44:55 +1000

    dns-app: output zone id

--- modules/dns-app/outputs.tf
+++ modules/dns-app/outputs.tf
@@ -2,3 +2,8 @@ output "cert_arn" {
   value       = module.acm_request_certificate.arn
   description = "Certificate ARN"
 }
+
+output "zone_id" {
+  value       = data.aws_route53_zone.existing.id
+  description = "Route53 zone ID"
+}
