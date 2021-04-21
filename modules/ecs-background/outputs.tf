ee7a6b727d0690f178e5dc30e728c1afdbdd5add
Joe Niland <joe@originalmind.com.au>
2021-04-21 21:21:16 +1000

    fix outputs

--- modules/ecs-background/outputs.tf
+++ modules/ecs-background/outputs.tf
@@ -1,11 +1,11 @@
 output "codepipeline_arn" {
-  value       = coalesce(concat([module.ecs_codepipeline.codepipeline_arn, module.ecs_codepipeline_skip_deploy.codepipeline_arn])...)
+  value       = module.ecs_codepipeline.codepipeline_arn
   description = "CodePipeline ARN"
 }
 
 output "ecs_service_security_group_id" {
   description = "Security Group ID of the ECS task"
-  value       = module.ecs_task.service_security_group_id
+  value       = module.ecs_alb_service_task.service_security_group_id
 }
 
 output "cloudwatch_log_group_name" {
