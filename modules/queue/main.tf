c47db94cf37dbe3a9331c2c5c213a3c2c6e12e4e
Joe Niland <joe@originalmind.com.au>
2021-04-21 20:18:49 +1000

    queue: expose queue access policy

--- modules/queue/main.tf
+++ modules/queue/main.tf
@@ -1,15 +1,15 @@
 // SQS
 data "aws_iam_policy_document" "sqs" {
-  count = (module.this.enabled && length(var.roles_for_queue_access) > 0) ? 1 : 0
+  count = module.this.enabled ? 1 : 0
 
-  # Allow specified roles to access queue messages
+  # Policy to allow access to queue messages
   statement {
     sid = ""
 
-    principals {
-      type        = "AWS"
-      identifiers = var.roles_for_queue_access
-    }
+    # principals {
+    #   type        = "AWS"
+    #   identifiers = var.roles_for_queue_access
+    # }
 
     actions = [
       "sqs:SendMessage",
@@ -25,16 +25,24 @@ data "aws_iam_policy_document" "sqs" {
   }
 }
 
-resource "aws_sqs_queue_policy" "sqs" {
-  count     = (module.this.enabled && length(var.roles_for_queue_access) > 0) ? 1 : 0
-  queue_url = module.queue.this_sqs_queue_id
-  policy    = join("", data.aws_iam_policy_document.sqs.*.json)
+resource "aws_iam_policy" "sqs_policy" {
+  count       = module.this.enabled ? 1 : 0
+  name        = module.this.id
+  path        = "/"
+  description = "Allow access to queue messages in ${module.this.id}"
+  policy      = data.aws_iam_policy_document.sqs.json
 }
+
+# resource "aws_sqs_queue_policy" "sqs" {
+#   count     = (module.this.enabled && length(var.roles_for_queue_access) > 0) ? 1 : 0
+#   queue_url = module.queue.this_sqs_queue_id
+#   policy    = join("", data.aws_iam_policy_document.sqs.*.json)
+# }
 
 module "queue" {
   source  = "terraform-aws-modules/sqs/aws"
   version = ">= 2.0"
   create  = module.this.enabled
-  name    = module.this.name
+  name    = module.this.id
   tags    = module.this.tags
 }
--- modules/queue/outputs.tf
+++ modules/queue/outputs.tf
@@ -2,10 +2,15 @@
 
 output "queue_arn" {
   description = "The ARN of the SQS queue"
-  value       = (module.this.enabled && length(var.roles_for_queue_access) > 0) ? module.queue.this_sqs_queue_arn : ""
+  value       = module.this.enabled ? module.queue.this_sqs_queue_arn : ""
 }
 
 output "queue_name" {
   description = "The name of the SQS queue"
-  value       = (module.this.enabled && length(var.roles_for_queue_access) > 0) ? module.queue.this_sqs_queue_name : ""
+  value       = module.this.enabled ? module.queue.this_sqs_queue_name : ""
+}
+
+output "queue_access_policy_arn" {
+  description = "Policy to allow access to queue"
+  value       = join("", aws_iam_policy.sqs_policy.*.arn)
 }
