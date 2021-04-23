e7387661aef95a5b0fd00bb771b9d49cc61b20d8
Joe Niland <joe@originalmind.com.au>
2021-04-23 19:56:47 +1000

    Add DeleteMessage to queue policy

--- modules/queue/main.tf
+++ modules/queue/main.tf
@@ -14,7 +14,8 @@ data "aws_iam_policy_document" "sqs" {
     actions = [
       "sqs:SendMessage",
       "sqs:ReceiveMessage",
-      "sqs:GetQueueUrl"
+      "sqs:GetQueueUrl",
+      "sqs:DeleteMessage"
     ]
 
     resources = [
@@ -32,12 +33,6 @@ resource "aws_iam_policy" "sqs_policy" {
   description = "Allow access to queue messages in ${module.this.id}"
   policy      = join("", data.aws_iam_policy_document.sqs.*.json)
 }
-
-# resource "aws_sqs_queue_policy" "sqs" {
-#   count     = (module.this.enabled && length(var.roles_for_queue_access) > 0) ? 1 : 0
-#   queue_url = module.queue.this_sqs_queue_id
-#   policy    = join("", data.aws_iam_policy_document.sqs.*.json)
-# }
 
 module "queue" {
   source  = "terraform-aws-modules/sqs/aws"
