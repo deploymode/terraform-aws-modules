7dce79672441e1819785a0db3be47994d7e0da2c
Joe Niland <joe@originalmind.com.au>
2021-05-19 16:57:42 +1000

    fix outputs

--- modules/queue/outputs.tf
+++ modules/queue/outputs.tf
@@ -2,12 +2,12 @@
 
 output "queue_arn" {
   description = "The ARN of the SQS queue"
-  value       = module.this.enabled ? module.queue.this_sqs_queue_arn : ""
+  value       = module.this.enabled ? module.queue.sqs_queue_arn : ""
 }
 
 output "queue_name" {
   description = "The name of the SQS queue"
-  value       = module.this.enabled ? module.queue.this_sqs_queue_name : ""
+  value       = module.this.enabled ? module.queue.sqs_queue_name : ""
 }
 
 output "queue_access_policy_arn" {
