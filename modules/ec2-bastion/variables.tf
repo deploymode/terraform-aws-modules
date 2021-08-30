1f1669aa81a50c7631c3d42c5fd8f972297b7141
Joe Niland <joe@originalmind.com.au>
2021-08-30 11:26:54 +1000

    ec2-bastion: remove unused vars

--- modules/ec2-bastion/variables.tf
+++ modules/ec2-bastion/variables.tf
@@ -1,13 +1,3 @@
-variable "region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  description = "List of Availability Zones where subnets will be created"
-}
-
 variable "zone_id" {
   type        = string
   default     = ""
