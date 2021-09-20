2f896b951560ca9e126e013ae71681751f7aa1ee
Joe Niland <joe@originalmind.com.au>
2021-09-20 15:28:35 +1000

    queue: output region

--- modules/queue/outputs.tf
+++ modules/queue/outputs.tf
@@ -14,3 +14,10 @@ output "queue_access_policy_arn" {
   description = "Policy to allow access to queue"
   value       = join("", aws_iam_policy.sqs_policy.*.arn)
 }
+
+data "aws_region" "current" {}
+
+output "queue_region" {
+  description = "SQS region"
+  value       = data.aws_region.current.name
+}
