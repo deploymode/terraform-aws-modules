81f084a6dd60c64ec8e138235499c8209887bd7d
Joe Niland <joe@originalmind.com.au>
2021-10-08 23:02:33 +1100

    account: include org resources

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -1,6 +1,22 @@
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = var.org_service_access_principals
+  enabled_policy_types          = var.org_enabled_policy_types
+  feature_set                   = var.org_feature_set
+}
+
+locals {
+  organization_root_account_id      = aws_organizations_organization.this.roots[0].id
+  organization_id                   = aws_organizations_organization.this.id
+  organization_arn                  = aws_organizations_organization.this.arn
+  organization_master_account_id    = aws_organizations_organization.this.master_account_id
+  organization_master_account_arn   = aws_organizations_organization.this.master_account_arn
+  organization_master_account_email = aws_organizations_organization.this.master_account_email
+}
+
 resource "aws_organizations_account" "account" {
-  name      = module.this.stage
-  email     = format(var.account_email, module.this.stage)
+  for_each  = toset(var.accounts)
+  name      = each.value.name
+  email     = format(var.account_email_format, each.key)
   role_name = var.account_role_name
 
   lifecycle {
--- modules/account/variables.tf
+++ modules/account/variables.tf
@@ -1,3 +1,21 @@
+variable "org_feature_set" {
+  type        = string
+  default     = "ALL"
+  description = "Organisation feature set. Specify \"ALL\" (default) or \"CONSOLIDATED_BILLING\"."
+}
+
+variable "org_service_access_principals" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS service principal names for which you want to enable integration with your organization. This is typically in the form of a URL, such as service-abbreviation.amazonaws.com. Organization must have feature_set set to ALL. For additional information, see the AWS Organizations User Guide."
+}
+
+variable "org_enabled_policy_types" {
+  type        = list(string)
+  description = "List of Organizations policy types to enable in the Organization Root. Organization must have feature_set set to ALL. For additional information about valid policy types (e.g. SERVICE_CONTROL_POLICY and TAG_POLICY), see the [AWS Organizations API Reference](https://docs.aws.amazon.com/organizations/latest/APIReference/API_EnablePolicyType.html)"
+  default     = []
+}
+
 variable "account_email" {
   type        = string
   description = "Account email format string. Used in combination with `name`, e.g. ops-%s@deploymode.com"
@@ -7,4 +25,9 @@ variable "account_role_name" {
   type        = string
   description = "IAM role that Organization automatically preconfigures in the new member account"
   default     = "OrganizationAccountAccessRole"
+}
+
+variable "accounts" {
+  type        = list(string)
+  description = "List of account names to create"
 }
