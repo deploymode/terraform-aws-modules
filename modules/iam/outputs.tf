831ce9c0c829c22b286cee3ee181da69dc29e374
Joe Niland <joe@originalmind.com.au>
2021-10-11 12:10:56 +1100

    iam: add outputs

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,94 +1,23 @@
-output "alb_name" {
-  description = "The ARN suffix of the ALB"
-  value       = module.alb.alb_name
+output "users" {
+  description = "IAM user name"
+  value = { for u in aws_iam_user.user : u.value.name => {
+    arn       = u.value.arn
+    unique_id = u.value.unique_id
+    }
+  }
 }
 
-output "alb_arn" {
-  description = "The ARN of the ALB"
-  value       = module.alb.alb_arn
+output "user_login_profile_encrypted_password" {
+  description = "The encrypted password, base64 encoded"
+  value = { for u in aws_iam_user_login_profile.user : u.value.name => {
+    encrypted_password = u.value.encrypted_password
+    key_fingerprint    = u.value.key_fingerprint
+    }
+  }
+  sensitive = true
 }
 
-output "alb_arn_suffix" {
-  description = "The ARN suffix of the ALB"
-  value       = module.alb.alb_arn_suffix
-}
-
-output "alb_dns_name" {
-  description = "DNS name of ALB"
-  value       = module.alb.alb_dns_name
-}
-
-output "alb_zone_id" {
-  description = "The ID of the zone which ALB is provisioned"
-  value       = module.alb.alb_zone_id
-}
-
-output "alb_security_group_id" {
-  description = "The security group ID of the ALB"
-  value       = module.alb.security_group_id
-}
-
-output "alb_default_target_group_arn" {
-  description = "The default target group ARN"
-  value       = module.alb.default_target_group_arn
-}
-
-output "alb_http_listener_arn" {
-  description = "The ARN of the HTTP listener"
-  value       = module.alb.http_listener_arn
-}
-
-output "alb_listener_arns" {
-  description = "A list of all the listener ARNs"
-  value       = module.alb.listener_arns
-}
-
-output "alb_access_logs_bucket_id" {
-  description = "The S3 bucket ID for access logs"
-  value       = module.alb.access_logs_bucket_id
-}
-
-output "cf_id" {
-  value       = module.cloudfront_s3_cdn.cf_id
-  description = "ID of AWS CloudFront distribution"
-}
-
-output "cf_arn" {
-  value       = module.cloudfront_s3_cdn.cf_arn
-  description = "ARN of AWS CloudFront distribution"
-}
-
-output "cf_status" {
-  value       = module.cloudfront_s3_cdn.cf_status
-  description = "Current status of the distribution"
-}
-
-output "cf_domain_name" {
-  value       = module.cloudfront_s3_cdn.cf_domain_name
-  description = "Domain name corresponding to the distribution"
-}
-
-output "cf_etag" {
-  value       = module.cloudfront_s3_cdn.cf_etag
-  description = "Current version of the distribution's information"
-}
-
-output "cf_hosted_zone_id" {
-  value       = module.cloudfront_s3_cdn.cf_hosted_zone_id
-  description = "CloudFront Route 53 zone ID"
-}
-
-output "cf_alias_hostnames" {
-  value       = var.aliases
-  description = "CloudFront DNS alias hostnames"
-}
-
-output "s3_bucket" {
-  value       = module.cloudfront_s3_cdn.s3_bucket
-  description = "Name of S3 bucket"
-}
-
-output "s3_bucket_domain_name" {
-  value       = module.cloudfront_s3_cdn.s3_bucket_domain_name
-  description = "Domain of S3 bucket"
+output "user_groups" {
+  description = "Group membership of users"
+  value       = aws_iam_user_group_membership.group_membership.*
 }
