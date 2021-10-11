2077288dac39b65acf88600fc18ad44844bc802f
Joe Niland <joe@originalmind.com.au>
2021-10-11 12:02:03 +1100

    add iam module

---
+++ modules/iam/main.tf
@@ -0,0 +1,64 @@
+#################################################################
+# Simplified IAM module for small orgs
+#
+#################################################################
+
+locals {
+  group_names = setproduct(toset(var.accounts), toset(var.groups))
+}
+
+resource "aws_iam_group" "group" {
+  for_each = var.group_names
+  name     = "${each.key[0]}-${each.key[1]}"
+}
+
+resource "aws_iam_user" "user" {
+  for_each      = var.users
+  name          = each.key
+  force_destroy = each.value.force_destroy
+  tags          = module.this.tags
+}
+
+resource "aws_iam_user_login_profile" "user_login" {
+  for_each = var.users
+  user     = aws_iam_user.user[each.key].name
+  pgp_key  = "keybase:${var.keybase_user}"
+}
+
+resource "aws_iam_group_membership" "group_membership" {
+  for_each = aws_iam_group.group # toset(var.groups)
+
+  name  = "${each.key}-membership"
+  users = [for u in var.users : u if contains(u.groups, each.value.name)]
+  group = each.value.name
+}
+
+
+data "aws_iam_policy_document" "assume_admin_role_with_mfa" {
+  for_each = var.accounts
+
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    resources = [
+      "arn:aws:iam::${each.value.account_id}:role/${each.value.org_access_role_name}",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_group_policy" "admin_group_with_mfa_dev" {
+  for_each = local_group_names
+  name     = "${each.key[0]}-${each.key[1]}-policy"
+  group    = "${each.key[0]}-${each.key[1]}"
+  policy   = data.aws_iam_policy_document.assume_admin_role_with_mfa[each.key[0]].json
+}
---
+++ modules/iam/variables.tf
@@ -0,0 +1,27 @@
+variable "accounts" {
+  type = map(object({
+    account_id = string
+  }))
+  description = "Map of account names to account IDs in the Org, e.g. master, prod"
+}
+
+variable "groups" {
+  type        = list(string)
+  default     = []
+  description = "List of IAM groups to create in account"
+}
+
+variable "users" {
+  type = map(object({
+    force_destroy = bool
+    groups        = list(string)
+  }))
+  default     = {}
+  description = "List of IAM users with assigned groups to create in account"
+}
+
+variable "keybase_user" {
+  type        = string
+  default     = ""
+  description = "Keybase username for encrypting IAM user passwords"
+}
