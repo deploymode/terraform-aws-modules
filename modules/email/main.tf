9e2708728b326f96538a4af056d923e951f24ce1
Joe Niland <joe@originalmind.com.au>
2021-10-18 16:01:57 +1100

    email: create role instead of using user/group

--- modules/email/main.tf
+++ modules/email/main.tf
@@ -7,10 +7,52 @@ module "ses" {
   source  = "cloudposse/ses/aws"
   version = "0.20.2"
 
-  domain        = var.domain
-  zone_id       = var.zone_id
-  verify_dkim   = var.verify_dkim
-  verify_domain = var.verify_domain
+  domain            = var.domain
+  zone_id           = var.zone_id
+  verify_dkim       = var.verify_dkim
+  verify_domain     = var.verify_domain
+  ses_user_enabled  = false
+  ses_group_enabled = false
 
   context = module.this.context
+}
+
+// Role to allow sending email via SES domain
+module "send_email_role" {
+  source  = "cloudposse/iam-role/aws"
+  version = "0.13.0"
+
+  context = module.this.context
+  name    = "email"
+
+  enabled = module.this.enabled
+
+  policy_document_count = 1
+
+  policy_description = "Allow sending email"
+  role_description   = "IAM role with send email permissions"
+
+  # Roles allowed to assume role
+  principals = {
+    AWS = [
+      "arn:aws:iam::${get_aws_account_id()}:root"
+    ]
+  }
+
+  policy_documents = [
+    data.aws_iam_policy_document.send_email_policy.json
+  ]
+}
+
+data "aws_iam_policy_document" "send_email_policy" {
+  statement {
+    actions = [
+      "ses:SendEmail",
+      "ses:SendRawEmail"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
 }
