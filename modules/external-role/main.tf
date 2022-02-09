da460ab1fdf7135cee8bf94fd7413107bbaa31b3
Joe Niland <joe@originalmind.com.au>
2022-02-09 14:48:51 +1100

    external-role: formatting

--- modules/external-role/main.tf
+++ modules/external-role/main.tf
@@ -5,7 +5,8 @@
 
 data "aws_iam_policy_document" "aws_managed_policies" {
   for_each = var.aws_policy_names
-  arn      = "arn:aws:iam::aws:policy/${each.key}"
+
+  arn = "arn:aws:iam::aws:policy/${each.key}"
 }
 
 module "role" {
