9a6f34b33e01b6484e3e8606bdf5fa5c425e9777
Joe Niland <joe@originalmind.com.au>
2022-03-11 15:53:53 +1100

    web: adjust json map ref

--- modules/ecs-web/main.tf
+++ modules/ecs-web/main.tf
@@ -308,7 +308,7 @@ module "ecs_task" {
       module.container_php-fpm.json_map_object
       ],
       var.monitoring_image_name != null ?
-      [module.container_monitoring.*.json_map_object] : []
+      [module.container_monitoring[0].json_map_object] : [P{}]
     )
   )
   ecs_cluster_arn              = var.ecs_cluster_arn
