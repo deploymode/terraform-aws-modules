b3b8bd9f4c9e18eb2dcebcd70eec735a9c2c5820
Joe Niland <joe@originalmind.com.au>
2022-05-13 08:19:44 +1000

    log-settings: modernisation/refactoring

--- modules/log-settings/main.tf
+++ modules/log-settings/main.tf
@@ -1,29 +1,6 @@
-// CloudWatch Log Group retention for CodeBuild and ECS Logs
-
-module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
-  namespace  = var.namespace
-  name       = var.app
-  stage      = var.stage
-  delimiter  = var.delimiter
-  attributes = var.attributes
-  tags       = var.tags
-}
-
-resource "null_resource" "ecs_log_retention" {
-  for_each = {
-    for k, v in var.ecs_log_groups :
-    k => v
-    if contains(var.log_groups_to_process, k)
-  }
-
+resource "null_resource" "log_retention" {
+  for_each = var.log_groups
   provisioner "local-exec" {
-    command = "aws logs put-retention-policy --log-group-name ${each.value} --retention-in-days ${var.ecs_log_group_retention_days}"
-  }
-}
-
-resource "null_resource" "codebuild_log_retention" {
-  provisioner "local-exec" {
-    command = "aws logs put-retention-policy --log-group-name /aws/codebuild/${module.label.id}-build --retention-in-days ${var.codebuild_log_group_retention_days}"
+    command = "aws logs put-retention-policy --log-group-name ${each.value.log_group_name} --retention-in-days ${each.value.retention_days}"
   }
 }
