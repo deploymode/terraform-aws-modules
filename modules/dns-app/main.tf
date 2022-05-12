7e38ebf694c7989f86a7ece8f14a29a30142bfd7
Joe Niland <joe@originalmind.com.au>
2022-05-12 19:21:16 +1000

    dns-app: fix domain list handling

--- modules/dns-app/main.tf
+++ modules/dns-app/main.tf
@@ -17,11 +17,12 @@ module "acm_request_certificate" {
   domain_name                       = var.cert_domain_name
   process_domain_validation_options = var.auto_verify
   ttl                               = "300"
-  subject_alternative_names = compact(concat(
-    formatlist("%s.%s", var.alternative_domain_prefixes, var.cert_domain_name),
-    count(var.alternative_domains) > 0 ? formatlist("%s.%s", var.alternative_domain_prefixes, var.alternative_domains) : [],
-    var.alternative_domains
-  ))
+  subject_alternative_names = formatlist("%s.%s", var.alternative_domain_prefixes,
+    compact(concat(
+      var.alternative_domains
+      [var.cert_domain_name]
+    ))
+  )
   wait_for_certificate_issued = var.wait_for_certificate_issued
   zone_name                   = data.aws_route53_zone.existing.name
 }
