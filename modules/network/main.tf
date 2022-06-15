31344cc5c9e89c7c852836ee43619b7d09bb4de1
Joe Niland <joe@originalmind.com.au>
2022-06-15 17:23:38 +1000

    network: update renamed vars

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,20 +1,20 @@
 module "vpc" {
-  source     = "cloudposse/vpc/aws"
-  version    = "1.1.0"
-  cidr_block = var.account_network_cidr
-  context    = module.this.context
+  source                  = "cloudposse/vpc/aws"
+  version                 = "1.1.0"
+  ipv4_primary_cidr_block = var.account_network_cidr
+  context                 = module.this.context
 }
 
 module "subnets" {
-  source                  = "cloudposse/dynamic-subnets/aws"
-  version                 = "2.0.2"
-  availability_zones      = var.zones
-  vpc_id                  = module.vpc.vpc_id
-  igw_id                  = module.vpc.igw_id
-  ipv4_primary_cidr_block = module.vpc.vpc_cidr_block
-  nat_gateway_enabled     = var.enable_nat_gateway
-  nat_instance_enabled    = var.enable_nat_instance
-  nat_instance_type       = var.nat_instance_type
+  source               = "cloudposse/dynamic-subnets/aws"
+  version              = "2.0.2"
+  availability_zones   = var.zones
+  vpc_id               = module.vpc.vpc_id
+  igw_id               = module.vpc.igw_id
+  ipv4_cidr_block      = module.vpc.vpc_cidr_block
+  nat_gateway_enabled  = var.enable_nat_gateway
+  nat_instance_enabled = var.enable_nat_instance
+  nat_instance_type    = var.nat_instance_type
   nat_elastic_ips = var.assign_elastic_ips ? [
     for az, eip in aws_eip.nat_ips : eip.public_ip
   ] : []
