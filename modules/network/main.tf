4f0ef91c3a1ed0bc637792ad30e1ffaf44c8e206
Joe Niland <joe@originalmind.com.au>
2022-06-15 17:29:41 +1000

    network: fix input var types

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -10,8 +10,8 @@ module "subnets" {
   version              = "2.0.2"
   availability_zones   = var.zones
   vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  ipv4_cidr_block      = module.vpc.vpc_cidr_block
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
   nat_gateway_enabled  = var.enable_nat_gateway
   nat_instance_enabled = var.enable_nat_instance
   nat_instance_type    = var.nat_instance_type
