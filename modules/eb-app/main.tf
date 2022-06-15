535a8d432eb90f66c9e5ca46a486ddd592633146
Joe Niland <joe@originalmind.com.au>
2022-06-15 17:18:26 +1000

    eb-app: initial commit

---
+++ modules/eb-app/main.tf
@@ -0,0 +1,92 @@
+module "elastic_beanstalk_application" {
+  source      = "cloudposse/elastic-beanstalk-application/aws"
+  description = var.description
+
+  context = module.this.context
+}
+
+data "aws_vpc" "default" {
+  vpc_id = var.vpc_id
+}
+
+module "elastic_beanstalk_environment" {
+  source = "cloudposse/elastic-beanstalk-environment/aws"
+
+  description                = var.description
+  region                     = var.region
+  availability_zone_selector = var.availability_zone_selector
+  dns_zone_id                = var.dns_zone_id
+
+  wait_for_ready_timeout             = var.wait_for_ready_timeout
+  elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
+  environment_type                   = var.environment_type
+  loadbalancer_type                  = var.loadbalancer_type
+  elb_scheme                         = var.elb_scheme
+  tier                               = var.tier
+  version_label                      = var.version_label
+  force_destroy                      = var.force_destroy
+
+  instance_type    = var.instance_type
+  root_volume_size = var.root_volume_size
+  root_volume_type = var.root_volume_type
+
+  autoscale_min             = var.autoscale_min
+  autoscale_max             = var.autoscale_max
+  autoscale_measure_name    = var.autoscale_measure_name
+  autoscale_statistic       = var.autoscale_statistic
+  autoscale_unit            = var.autoscale_unit
+  autoscale_lower_bound     = var.autoscale_lower_bound
+  autoscale_lower_increment = var.autoscale_lower_increment
+  autoscale_upper_bound     = var.autoscale_upper_bound
+  autoscale_upper_increment = var.autoscale_upper_increment
+
+  vpc_id               = var.vpc_id
+  loadbalancer_subnets = var.public_subnet_ids
+  application_subnets  = var.private_subnet_ids
+
+  allow_all_egress = true
+
+  additional_security_group_rules = [
+    {
+      type                     = "ingress"
+      from_port                = 0
+      to_port                  = 65535
+      protocol                 = "-1"
+      source_security_group_id = data.aws_vpc.default.vpc_default_security_group_id
+      description              = "Allow all inbound traffic from trusted Security Groups"
+    }
+  ]
+
+  rolling_update_enabled  = var.rolling_update_enabled
+  rolling_update_type     = var.rolling_update_type
+  updating_min_in_service = var.updating_min_in_service
+  updating_max_batch      = var.updating_max_batch
+
+  healthcheck_url  = var.healthcheck_url
+  application_port = var.application_port
+
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
+  solution_stack_name = var.solution_stack_name
+
+  additional_settings = var.additional_settings
+  env_vars            = var.env_vars
+
+  extended_ec2_policy_document = data.aws_iam_policy_document.minimal_s3_permissions.json
+  prefer_legacy_ssm_policy     = false
+  prefer_legacy_service_policy = false
+  scheduled_actions            = var.scheduled_actions
+
+  context = module.this.context
+}
+
+data "aws_iam_policy_document" "minimal_s3_permissions" {
+  statement {
+    sid = "AllowS3OperationsOnElasticBeanstalkBuckets"
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation"
+    ]
+    resources = ["*"]
+  }
+}
---
+++ modules/eb-app/outputs.tf
@@ -0,0 +1,4 @@
+output "elastic_beanstalk_application_name" {
+  value       = module.elastic_beanstalk_application.elastic_beanstalk_application_name
+  description = "Elastic Beanstalk Application name"
+}
