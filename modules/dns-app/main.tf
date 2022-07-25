61f7b4aec0b37a19efbdb16a7ba371c044556634
Joe Niland <joe@originalmind.com.au>
2022-07-25 15:00:23 +1000

    dns-app:
    - bump upstream module
    - fix handling of alternate domains and prefixes

--- modules/dns-app/main.tf
+++ modules/dns-app/main.tf
@@ -12,16 +12,24 @@ data "aws_route53_zone" "existing" {
 }
 
 module "acm_request_certificate" {
-  source                            = "git::https://github.com/cloudposse/terraform-aws-acm-request-certificate.git?ref=tags/0.16.0"
+  source                            = "cloudposse/acm-request-certificate/aws"
+  version                           = "0.16.0"
   enabled                           = module.this.enabled
   domain_name                       = var.cert_domain_name
+  validation_method                 = var.validation_method
   process_domain_validation_options = var.auto_verify
   ttl                               = "300"
-  subject_alternative_names = formatlist("%s.%s", var.alternative_domain_prefixes,
-    compact(concat(
-      var.alternative_domains,
-      [var.cert_domain_name]
-    ))
+  subject_alternative_names = length(var.alternative_domain_prefixes) == 0 ? var.alternative_domains : concat(
+    var.alternative_domains,
+    flatten([
+      for prefix in var.alternative_domain_prefixes :
+      formatlist("%s.%s", prefix,
+        compact(concat(
+          var.alternative_domains,
+          [var.cert_domain_name]
+        ))
+      )
+    ])
   )
   wait_for_certificate_issued = var.wait_for_certificate_issued
   zone_name                   = data.aws_route53_zone.existing.name
--- modules/dns-app/variables.tf
+++ modules/dns-app/variables.tf
@@ -1,17 +1,17 @@
-variable "hosted_zone_name" {
-  type        = string
-  description = "Route53 hosted zone name. Defaults to `domain_name` if value is \"\"."
-  default     = ""
-}
-
 variable "cert_domain_name" {
   type        = string
   description = "Domain name for ACM cert"
 }
 
+variable "hosted_zone_name" {
+  type        = string
+  description = "Route53 hosted zone name. Defaults to `cert_domain_name` if value is empty."
+  default     = ""
+}
+
 variable "auto_verify" {
   type        = bool
-  description = "Set to true if Terraform should perform certification validation. This will also create a hosted zone."
+  description = "Set to true if Terraform should perform certification validation. This will also create a hosted zone. Ignored if validation_method is not \"DNS\"."
   default     = false
 }
 
@@ -23,14 +23,18 @@ variable "wait_for_certificate_issued" {
 
 variable "alternative_domain_prefixes" {
   type        = list(string)
-  description = "A list of subdomains of domain_name that should be SANs in the issued certificate"
-  default = [
-  ]
+  description = "A list of subdomains of domain_name or '*' that should be SANs in the issued certificate"
+  default     = []
 }
 
 variable "alternative_domains" {
   type        = list(string)
   description = "A list of domain names that should be SANs in the issued certificate"
-  default = [
-  ]
+  default     = []
+}
+
+variable "validation_method" {
+  type        = string
+  default     = "DNS"
+  description = "Method to use for validation, DNS or EMAIL"
 }
