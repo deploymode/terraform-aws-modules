fcab28cc9cbc7ac5ba960ec7d4e2509d8b0c211c
Joe Niland <joe@originalmind.com.au>
2023-07-26 16:35:09 +1000

    dns-primary: support additional arbitrary dns records on the primary zone

--- modules/dns-primary/main.tf
+++ modules/dns-primary/main.tf
@@ -1,7 +1,12 @@
+
+
 # Primary DNS zone to be delegated to other accounts
+# This module is pretty much a straight copy of https://github.com/cloudposse/terraform-aws-components/blob/main/modules/dns-primary/main.tf
+
 locals {
   dns_soa_config = "awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
   domains_set    = toset(var.domains)
+  zone_recs_map  = { for zone in var.record_config : "${zone.name}${zone.root_zone}.${zone.type}" => zone }
 }
 
 resource "aws_route53_zone" "root" {
@@ -24,4 +29,15 @@ resource "aws_route53_record" "soa" {
   records = [
     "${aws_route53_zone.root[each.key].name_servers[0]}. ${local.dns_soa_config}"
   ]
+}
+
+resource "aws_route53_record" "dnsrec" {
+  for_each = local.zone_recs_map
+
+  name    = format("%s%s", each.value.name, each.value.root_zone)
+  type    = each.value.type
+  zone_id = aws_route53_zone.root[each.value.root_zone].zone_id
+  ttl     = each.value.ttl
+
+  records = each.value.records
 }
--- modules/dns-primary/variables.tf
+++ modules/dns-primary/variables.tf
@@ -3,3 +3,15 @@ variable "domains" {
   description = "Domains to set up zones for"
   default     = []
 }
+
+variable "record_config" {
+  description = "DNS record config for additional DNS records to be added to the primary zone"
+  type = list(object({
+    root_zone = string
+    name      = string
+    type      = string
+    ttl       = string
+    records   = list(string)
+  }))
+  default = []
+}
