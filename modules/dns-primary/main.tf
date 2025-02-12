1304e62558b4b64eaf29ea4ff1686bd7c4e576ff
Joe Niland <joe@originalmind.com.au>
2025-02-12 17:29:43 +1100

    dns-primary: add DS keys to outputs
    - dns-primary: temporarily use our fork

--- modules/dns-primary/main.tf
+++ modules/dns-primary/main.tf
@@ -1,11 +1,11 @@
 locals {
   dns_soa_config = "awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
   zone_recs_map  = { for zone in var.record_config : "${zone.name}${zone.root_zone}.${zone.type}" => zone }
-  dnssec_zones = {for k, v in var.domains: k => v if v.dnssec_enabled == true}
+  dnssec_zones   = { for k, v in var.domains : k => v if v.dnssec_enabled == true }
 }
 
 resource "aws_route53_zone" "root" {
-  for_each = var.domains 
+  for_each = var.domains
 
   name    = each.key
   comment = "DNS zone for the ${each.key} root domain"
@@ -38,8 +38,9 @@ resource "aws_route53_record" "dnsrec" {
 }
 
 module "dnssec" {
-  source  = "ugns/route53-dnssec/aws"
-  version = "1.1.0"
+  # source  = "ugns/route53-dnssec/aws"
+  # version = "1.1.0"
+  source = "git::https://github.com/deploymode/terraform-aws-route53-dnssec.git?ref=tags/1.1.1"
 
   for_each = local.dnssec_zones
 
--- modules/dns-primary/outputs.tf
+++ modules/dns-primary/outputs.tf
@@ -2,3 +2,8 @@ output "zones" {
   value       = aws_route53_zone.root
   description = "DNS zones"
 }
+
+output "ds_records" {
+  value       = { for zone, records in module.dnssec : zone => records["ds_record"] }
+  description = "DS records"
+}
