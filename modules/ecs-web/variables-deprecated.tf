02e525c1a8c7c8b0aa3c0d5e469220bde9982a61
Joe Niland <joe@originalmind.com.au>
2025-03-29 11:56:39 +1100

    fix: ecs-web - incorrect validation

--- modules/ecs-web/variables-deprecated.tf
+++ modules/ecs-web/variables-deprecated.tf
@@ -5,7 +5,7 @@ variable "external_app_buckets" {
   description = "Existing S3 buckets used by the application. Allows application and CodePipeline roles to access these buckets."
   default     = []
   validation {
-    condition     = length(var.external_app_buckets) >= 1
+    condition     = length(var.external_app_buckets) == 0
     error_message = "This variable has been deprecated. Manage S3 access using `ecs_task_policy_arns` and `codebuild_policy_arns`"
   }
 }
