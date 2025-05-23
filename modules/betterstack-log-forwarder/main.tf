fc0c953dd8024f50e1186054d63eb07f6b75cbf3
Joe Niland <joe@originalmind.com.au>
2025-05-23 14:43:49 +1000

    fix(betterstack-log-forwarder): typo in module url

--- modules/betterstack-log-forwarder/main.tf
+++ modules/betterstack-log-forwarder/main.tf
@@ -1,5 +1,5 @@
 module "log_forwarder" {
-  source = "git::https://github.com/deploymode/terraform-betterstack-cloudwatch-logs.git?ref=0.1.1"
+  source = "git::https://github.com/deploymode/terraform-betterstack-cloudwatch-logs.git?ref=tags/0.1.1"
 
   better_stack_token       = var.better_stack_token
   better_stack_ingest_host = var.better_stack_ingest_host
